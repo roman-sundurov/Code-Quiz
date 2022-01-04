bc8bf884694add9ce596e5a7eacbe53efd37cfc6
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-04 12:16:48 +0300

    Initial Commit

---
+++ Code Quiz/Code_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_QuizApp.swift
+//  Code Quiz
+//
+//  Created by Roman on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Code_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
