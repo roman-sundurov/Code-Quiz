baa6cc8f2d79175f0cf00000c0b7a2ec613ba502
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-09 00:54:11 +0300

    Finished: updating the user interface.

--- Code Quiz/ContentView.swift
+++ Code Quiz/ContentView.swift
@@ -13,9 +13,7 @@ struct ContentView: View {
             questionText: "What was the first computer bug?",
             possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
             correctAnswerIndex: 2)
-
-    
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
 
     var body: some View {
         
@@ -32,34 +30,45 @@ struct ContentView: View {
                    .bold()
                    .multilineTextAlignment(.leading)
                 Spacer()
-                HStack {
-                    Button(action: {
-                        print("Tapped on Choice 1")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[0])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 2")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 3")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    })
-                    Button(action: {
-                        print("Tapped on Choice 4")
-                    }, label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-                    })
-                }
-
                 
+                HStack {
+                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        })
+                    }
+//                    Button(action: {
+//                        print("Tapped on Choice 1")
+//                    }, label: {
+//                        ChoiceTextView(choiceText: question.possibleAnswers[0])
+//                    })
+//                    Button(action: {
+//                        print("Tapped on Choice 2")
+//                    }, label: {
+//                        ChoiceTextView(choiceText: question.possibleAnswers[1])
+//                    })
+//                    Button(action: {
+//                        print("Tapped on Choice 3")
+//                    }, label: {
+//                        ChoiceTextView(choiceText: question.possibleAnswers[2])
+//                    })
+//                    Button(action: {
+//                        print("Tapped on Choice 4")
+//                    }, label: {
+//                        ChoiceTextView(choiceText: question.possibleAnswers[3])
+//                    })
+//                    Button(action: {
+//                        print("Tapped on Choice 4")
+//                    }, label: {
+//                        ChoiceTextView(choiceText: "wwwwwwwwww")
+//                    })
+                }
             }
         }
         .foregroundColor(.white)
-        
     }
     
 }
