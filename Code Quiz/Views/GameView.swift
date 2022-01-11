70f9ea47b36e28034eaeeb350c4818f3f6341fe7
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-01-12 02:24:29 +0300

    Finished: finished creating the GameViewModel.

--- Code Quiz/Views/GameView.swift
+++ Code Quiz/Views/GameView.swift
@@ -8,70 +8,26 @@
 import SwiftUI
 
 struct GameView: View {
-    
-    let question = Question(
-            questionText: "What was the first computer bug?",
-            possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
-            correctAnswerIndex: 2)
-    @State var mainColor = GameColor.main
-
-    var body: some View {
-        
-        ZStack {
-            mainColor.ignoresSafeArea()
-            VStack{
-                
-                Text("1 / 10")
-                    .font(.callout)
-                    .multilineTextAlignment(.leading)
-                    .padding()
-                Text(question.questionText)
-                    .font(.largeTitle)
-                   .bold()
-                   .multilineTextAlignment(.leading)
-                Spacer()
-                
-                HStack {
-                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
-                        Button(action: {
-                            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
-                        }, label: {
-                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-                        })
-                    }
-//                    Button(action: {
-//                        print("Tapped on Choice 1")
-//                    }, label: {
-//                        ChoiceTextView(choiceText: question.possibleAnswers[0])
-//                    })
-//                    Button(action: {
-//                        print("Tapped on Choice 2")
-//                    }, label: {
-//                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-//                    })
-//                    Button(action: {
-//                        print("Tapped on Choice 3")
-//                    }, label: {
-//                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-//                    })
-//                    Button(action: {
-//                        print("Tapped on Choice 4")
-//                    }, label: {
-//                        ChoiceTextView(choiceText: question.possibleAnswers[3])
-//                    })
-//                    Button(action: {
-//                        print("Tapped on Choice 4")
-//                    }, label: {
-//                        ChoiceTextView(choiceText: "wwwwwwwwww")
-//                    })
-                }
-            }
-        }
-        .foregroundColor(.white)
+ 
+  @StateObject var viewModel = GameViewModel()
+ 
+  var body: some View {
+    ZStack {
+      GameColor.main.ignoresSafeArea()
+      VStack {
+        Text(viewModel.questionProgressText)
+          .font(.callout)
+          .multilineTextAlignment(.leading)
+          .padding()
+        QuestionView(question: viewModel.currentQuestion)
+      }
     }
-    
+    .foregroundColor(.white)
+    .navigationBarHidden(true)
+    .environmentObject(viewModel)
+  }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
